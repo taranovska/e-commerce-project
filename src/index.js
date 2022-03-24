ed49283366fe6418526a1864b1c9010040f9f07a
taranovska <93533926+taranovska@users.noreply.github.com>
2022-03-24 12:23:56 +0200

    added stripe

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-c5vbywdm.us.auth0.com"
-    clientId="NEa05Wr7t70K3pwcYiC4ydvJnh1bNdjh"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,7 +5,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 //import { useUserContext } from "../context/user_context";
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const { user } = useAuth0;
+  const { user } = useAuth0();
   return (
     <Route
       {...rest}
